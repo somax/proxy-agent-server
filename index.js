320355d89fa78d33d6c22775fda3b5432b89b7a8
somax <somaxj@163.com>
2017-12-01 10:54:46 +0800

    chore: console log

--- index.js
+++ index.js
@@ -8,19 +8,23 @@ const version = require('./package.json').version
 const hostname = '127.0.0.1';
 const port = process.argv[2] || 1090;
 
-const proxyUri = process.env.socks_proxy || 'socks5://127.0.0.1:57197';
+const proxyUri = process.env.socks_proxy || 'socks5://127.0.0.1:1080';
 
 const server = http.createServer((req, res) => {
+    console.log(`${req.method} ${req.url}`);
     request({
         url: req.url,
         headers: req.headers,
         method: req.method,
         agent: new ProxyAgent(proxyUri)
-    }).on('error', function (err) {
+    })
+    .on('data', buf => console.log(`Data length: ${buf.length}`))
+    .on('error', function (err) {
         console.log(err);
         res.statusCode = 500;
         res.end(`PAS Error: ${err.toString()}`);
-    }).pipe(res);
+    })
+    .pipe(res);
 
 });
 
